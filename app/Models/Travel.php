97f09e42776ca21c9351a80fb7cfcd17f602e69e
Luis Urán <luisuran.bit@gmail.com>
2023-06-10 11:25:16 -0300

    added fields completion for travel

--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Travel extends Model
 {
@@ -16,6 +17,14 @@ class Travel extends Model
         'description',
         'number_of_days',
     ];
+
+    protected static function booted(): void
+    {
+        static::saving(function (Travel $travel): void {
+            $travel->slug = Str::of($travel->name)->slug('-');
+            $travel->number_of_nights = $travel->number_of_days - 1;
+        });
+    }
 
     public function tours()
     {
